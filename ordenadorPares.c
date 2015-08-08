6156c95e339dadbb2fa35b26ed483a1c7d8f1216
marcelo meneses <menesesmarcelo1@gmail.com>
2015-08-08 18:33:31 -0300

    Update ordenadorPares.c

--- ordenadorPares.c
+++ ordenadorPares.c
@@ -1,3 +1,20 @@
+/* Copyright 2015 Marcelo Meneses de Lima.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 /*
  * ordenadorPares.c
  *
@@ -51,11 +68,11 @@ int main(void) {
 			6, 7, 0, 1, 1, 7, 4, 7, 0, 8, 8, 4, 7, 3, 8, 8, 5, 9, 4, 0, 8, 2, 0,
 			8, 1, 6, 4, 6, 7, 9, 9, 5, 6, 1, 6, 8, 9, 3, 7, 1, 3, 8, 5, 0, 1, 5,
 			1, 7, 6, 7, 9, 5, 9, 9, 5, 2, 6, 1, 8, 5, 1, 9, 0, 7, 0, 8, 7, 1, 1,
-			7, 4, 5, 5, 1, 7, 8, 6, 0, 7, 3, 7, 7, 0, 8 };
+			7, 4, 5, 5, 1, 7, 8, 6, 0, 7, 3, 7, 7, 0 };
 	//size of array:
-	//array[1000] => arrs=999
-	//array[500] => arrs=499
-	//array[5] => arrs=4
+	//array[1000] => arrs = 999
+	//array[500] => arrs = 499
+	//array[5] => arrs = 4
 	arrs = 999;
 
 	a = 1;
